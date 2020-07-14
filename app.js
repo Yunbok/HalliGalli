2894765dee8ab92056be99442dc0a83bd0e9fbef
NoI <qodbsqhr@gmail.com>
2020-07-14 09:20:11 +0000

    최초

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http'); // node 내장 모듈 불러옴
+
+//var hostname = '127.0.0.1'; // localhost와 동일
+var port = 3000;
+
+http.createServer(function(req, res){
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
+
+console.log('Server running at http://:'+port);
